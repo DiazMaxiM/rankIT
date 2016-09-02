3bd70f50a38352872dd1ae0f57c84f57c7a158d0
rosali <zaracho.rosali@gmail.com>
2016-09-02 13:50:55 -0300

    trabajando con lugar window

--- rankit-ui-arena/src/main/generated-sources/xtend/edu/unq/interfaces/rankit_Interfaz/AdmLugarWindow.java
+++ rankit-ui-arena/src/main/generated-sources/xtend/edu/unq/interfaces/rankit_Interfaz/AdmLugarWindow.java
@@ -1,5 +1,26 @@
 package edu.unq.interfaces.rankit_Interfaz;
 
+import edu.unq.interfaces.rankit_dominio.Lugar;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+
 @SuppressWarnings("all")
-public class AdmLugarWindow {
+public class AdmLugarWindow extends SimpleWindow<Lugar> {
+  public AdmLugarWindow(final WindowOwner parent, final Lugar model) {
+    super(parent, model);
+    this.setTitle("Rank-IT -->Adm Servicios");
+    this.setTaskDescription("Resumen de situación");
+  }
+  
+  protected void createFormPanel(final Panel panelPrincipal) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method primerLayout(Panel) is undefined for the type AdmLugarWindow"
+      + "\nThe method segundoLayout(Panel) is undefined for the type AdmLugarWindow"
+      + "\nThe method tercerLayout(Panel) is undefined for the type AdmLugarWindow");
+  }
+  
+  protected void addActions(final Panel arg0) {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
 }
--- rankit-ui-arena/src/main/java/edu/unq/interfaces/rankit_Interfaz/AdmLugarWindow.xtend
+++ rankit-ui-arena/src/main/java/edu/unq/interfaces/rankit_Interfaz/AdmLugarWindow.xtend
@@ -1,5 +1,29 @@
 package edu.unq.interfaces.rankit_Interfaz
 
-class AdmLugarWindow {
+import org.uqbar.arena.windows.SimpleWindow
+import org.uqbar.arena.windows.WindowOwner
+import edu.unq.interfaces.rankit_dominio.Lugar
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.layout.VerticalLayout
+
+class AdmLugarWindow extends SimpleWindow<Lugar>{
+	
+	new(WindowOwner parent, Lugar model) {
+		super(parent, model)
+		title = "Rank-IT -->Adm Servicios"
+	    taskDescription = "Resumen de situación"
+	    
+	}
+	
+	override protected createFormPanel(Panel panelPrincipal) {
+	   	this.primerLayout(panelPrincipal)
+	   	this.segundoLayout(panelPrincipal)
+	    this.tercerLayout(panelPrincipal)
+
+	}
+	
+	override protected addActions(Panel arg0) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
 	
 }
