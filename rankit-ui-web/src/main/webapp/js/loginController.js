b0f0330bcb9c2de24722c32b00524964c44a1427
LizaChambi <liza.chambi@gmail.com>
2016-10-20 17:13:45 -0300

    completando el loginController

--- rankit-ui-web/src/main/webapp/js/loginController.js
+++ rankit-ui-web/src/main/webapp/js/loginController.js
@@ -1,15 +1,40 @@
-rankitApp.controller('LoginController', function ($resource) {
-	 var self = this;
-	 this.logeado = false
-	 this.error =""
-	 this.logear =function(){}
+rankitApp.controller('LoginController', function ($resource) 
+{
+	var app = angular.module('RegistroController', []);
+	
+	app.controller('RegistroCtrl', function ($scope) 
+	{	
+	    $scope.usuariosRegistrados =[{nombre:"Camila", contrasenha:"1234"}, {nombre:"Liza", contrasenha:"5678"}];
+		$scope.nombre ="";
+		$scope.cantUsuarios = function() 
+		{
+			return $scope.usuariosRegistrados.length;
+		}
+		
+		scope.loguearseSiPuede = function() 
+		{
+			if (puedoLoguearme())
+			{
+				loguear()
+			}
+			else
+			{
+				noHayRegistroValido()
+			}
+		}
+			
+	 //var self = this;
+	 //this.logeado = false
+	 //this.error =""
+	 //this.logear =function(){}
 
-	 this.loginOk = function (data){
-	 	self.logeado = true
+	 //this.loginOk = function (data){
+	 //	self.logeado = true}
+	 
+	 //this.errorLogin = function (data){
+	 //	self.logeado=false
+	 //	modalError("Error Login",data.error)}
+		
+	}
+);
 
-	 }
-	 this.errorLogin = function (data){
-	 	self.logeado=false
-	 	modalError("Error Login",data.error)
-	 }
-});
