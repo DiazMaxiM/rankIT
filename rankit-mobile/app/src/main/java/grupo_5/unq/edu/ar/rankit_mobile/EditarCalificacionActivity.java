c79f6e0d2f2940be53f5775253bb3c8bd3a14ca6
rozaracho <zaracho.rosali@gmail.com>
2016-11-06 11:21:51 -0300

    empezando con android :(

---
+++ rankit-mobile/app/src/main/java/grupo_5/unq/edu/ar/rankit_mobile/EditarCalificacionActivity.java
@@ -0,0 +1,13 @@
+package grupo_5.unq.edu.ar.rankit_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarCalificacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_calificacion);
+    }
+}
