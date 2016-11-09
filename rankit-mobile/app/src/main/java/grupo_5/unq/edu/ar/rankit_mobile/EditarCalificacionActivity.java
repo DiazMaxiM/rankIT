7b2cfc14a3b2df691f7a793467edb2ace80986de
rozaracho <zaracho.rosali@gmail.com>
2016-11-09 17:09:28 -0300

    boton atras

--- rankit-mobile/app/src/main/java/grupo_5/unq/edu/ar/rankit_mobile/EditarCalificacionActivity.java
+++ rankit-mobile/app/src/main/java/grupo_5/unq/edu/ar/rankit_mobile/EditarCalificacionActivity.java
@@ -1,5 +1,6 @@
 package grupo_5.unq.edu.ar.rankit_mobile;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class EditarCalificacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_calificacion);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
